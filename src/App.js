e89b1c15a705bc4cdea05e374621ba7e6d91cbf7
Iasmin <iasminraquelsou@gmail.com>
2023-01-24 13:56:47 -0300

    using State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 import Relogio from "./componentes/Relogio";
+import Numero from "./componentes/Numero";
 import './App.css'
 
 export default function App() {
   
+  const [num,setNum] = useState(10)
+
   const textoDestaque={
     fontSize: '2rem',
     fontStyle: 'italic'
@@ -16,6 +19,9 @@ export default function App() {
         <h1 style={{color:'red', fontSize:'5rem', margin:'0'}}>React</h1>
         <h2 style={textoDestaque}>Estilização de componentes</h2>
         <p className="texto">Editando o CSS do aplicativo</p>
+        <p>O valor do state: {num}</p>
+        <button onClick={() => setNum(100)} >100</button>
+        <Numero num={num} setNum={setNum}/>
       </section>
     </>
   )
---
+++ src/componentes/Numero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Numero(props) {
+  return(
+    <>
+      <p>Numero: {props.num}</p>
+      <button onClick={()=>{props.setNum(props.num+10)}}>Soma 10</button> 
+    </>
+  )
+}
