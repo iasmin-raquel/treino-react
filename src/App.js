b4c72b42a2850d0ae1902a7d1cb6344cb28441b1
Iasmin <iasminraquelsou@gmail.com>
2023-01-24 16:02:24 -0300

    listas

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import React,{useState} from "react";
+import React from "react";
 
 export default function App() {
 
-  const [cor,setCor]=useState(1)
+  const carros = ['crossfox', 'onix', 'hilux', 'rangerhover', 'uno']
 
-  const vermelho = {color:"#f00"}
-  const verde = {color:"#0f0"}
-  const azul = {color:"#00f"}
-
-  const retCor = (c)=>{
-    if (c===1) {
-      return vermelho
-    } else if (c===2) {
-      return verde
-    } else {
-      return azul
-    }
-  }
-
-  const mudaCor = () => {
-    setCor(cor+1)
-    if(cor>2) {
-      setCor(1)
-    }
-  }
-
-  //setInterval(mudaCor, 1000)
+  const listaCarros = carros.map(
+    (c,i) => <li>{i} - {c}</li>
+  )
 
   return(
     <>
-      <h1 style={retCor(cor)}>Iasmin Raquel</h1>
-      <button onClick={()=>mudaCor()} >mudar cor</button>
+      <ul>{listaCarros}</ul>
     </>
   )
 }
