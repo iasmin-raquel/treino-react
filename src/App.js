81d4efdb5a26af348c8a385c6b56ac1d75322414
Iasmin <iasminraquelsou@gmail.com>
2023-01-26 14:05:15 -0300

    componentes de classe

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
+import Classe from "./componentes/Classe";
 
 export default function App() {
-  const [nome, setNome] = useState()
-
-  const armazenar = (chave, item) => {
-    localStorage.setItem(chave, item)
-  }
-
-  const verNome = (chave) => {
-    alert(localStorage.getItem(chave))
-  }
-
-  const apagar = (chave) => {
-    localStorage.removeItem(chave)
-  } 
 
   return(
     <>
-      <label>Digite seu nome: </label><br/>
-      <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>
-      <button onClick={()=>armazenar('ls_nome', nome)} >Armazenar</button>
-      <button onClick={()=>verNome('ls_nome')} >Ver nome</button>
-      <button onClick={()=>apagar('ls_nome')} >Remover Nome</button>
-
+      <h1>Componentes de Classe</h1>
+      <Classe nome="iasmin" ></Classe>
     </>
   )
 }
