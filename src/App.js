e88974242dbebcf5c33f42f55871edecb7bf24d3
Iasmin <iasminraquelsou@gmail.com>
2023-01-26 13:30:21 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import { Notas } from "./componentes/Notas";
+import React, {useEffect, useState} from "react";
 
 export default function App() {
+  const [contagem, setContagem] = useState(0)
+  
+  useEffect(
+    () => {
+      console.log('página carregada')
+      document.title = 'Contagem ' + contagem
+    }
+  )
+
   return(
     <>
-      <Notas>
-        <p>Iasmin</p>
-      </Notas>
+      <p>Contagem: {contagem}</p>
+      <button onClick={()=>{setContagem(contagem+1)}} >Contar</button>
     </>
   )
 }
